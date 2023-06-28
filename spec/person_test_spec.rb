8119cf14f2fc4cc887a5e39d1c17ad00d00a76cc
Amarachi Dimkpa <amaradimkpa1@gmail.com>
2023-06-28 14:30:00 +0100

    Add the test for person

--- spec/person_test_spec.rb
+++ spec/person_test_spec.rb
@@ -2,31 +2,63 @@ require_relative '../classes/person'
 require_relative '../classes/rental'
 require_relative '../classes/book'
 
-describe Person do
-  context 'Create an instance of person and test methods' do
-    it 'Create an instance of the class person with parameters name and parent permission as default' do
-      person = Person.new(22)
-      expect(person).to be_a Person
+RSpec.describe Person do
+  let(:age) { 20 }
+  let(:name) { 'John Doe' }
+  let(:parent_permission) { true }
+  subject { described_class.new(age, name, parent_permission: parent_permission) }
+
+  describe '#initialize' do
+    it 'creates a new person with the specified age, name, and parent permission' do
+      expect(subject.age).to eq(age)
+      expect(subject.name).to eq(name)
+      expect(subject.rentals).to be_empty
     end
 
-    # it 'Test the correct name method' do
-    #   person = Person.new(22)
-    #   correct = person.correct_name
-    #   expect(correct).to eq 'Unknown'
-    #   expect(person.parent_permission).to eq true
-    # end
+    it 'generates a random ID between 1 and 2000' do
+      expect(subject.id).to be_between(1, 2000).inclusive
+    end
+  end
 
-    it 'Test can use services method' do
-      person = Person.new(23, 'Hika')
-      use_service = person.can_use_services?
-      expect(use_service).to eq true
+  describe '#correct_name' do
+    it 'returns the name as-is' do
+      expect(subject.correct_name).to eq(name)
+    end
+  end
+
+  describe '#can_use_services?' do
+    context 'when the person is of age' do
+      let(:age) { 25 }
+
+      it 'returns true' do
+        expect(subject.can_use_services?).to be true
+      end
     end
 
-    it 'Test add rental method' do
-      person = Person.new(34, 'Lils')
-      book = Book.new('the dark', 'Myles')
-      rent = Rental.new('2021-11-22', book, person)
-      expect(person.rentals).to include(rent)
+    context 'when the person is underage with parent permission' do
+      let(:age) { 15 }
+
+      it 'returns true' do
+        expect(subject.can_use_services?).to be true
+      end
+    end
+
+    context 'when the person is underage without parent permission' do
+      let(:age) { 15 }
+      let(:parent_permission) { false }
+
+      it 'returns false' do
+        expect(subject.can_use_services?).to be false
+      end
+    end
+  end
+
+  describe '#add_rental' do
+    let(:rental) { double('Rental') }
+
+    it 'adds the rental to the person' do
+      subject.add_rental(rental)
+      expect(subject.rentals).to include(rental)
     end
   end
 end
